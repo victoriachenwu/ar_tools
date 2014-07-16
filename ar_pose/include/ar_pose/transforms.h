b781f3142add1cd0a21f4d046f49f92f90e725c3
vptarmigan <victoria.chen.wu@gmail.com>
2014-07-16 13:42:45 -0400

    forgot transforms header file

---
+++ ar_pose/include/ar_pose/transforms.h
@@ -0,0 +1,36 @@
+/*
+ *  AR Objects
+ *  Copyright (C) 2013, I Heart Engineering
+ *  Copyright (C) 2010, CCNY Robotics Lab
+ *  William Morris <bill@iheartengineering.com>
+ *  Ivan Dryanovski <ivan.dryanovski@gmail.com>
+ *  Gautier Dumonteil <gautier.dumonteil@gmail.com>
+ *  http://www.iheartengineering.com
+ *  http://robotics.ccny.cuny.edu
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRANSFORMS_H 
+#define TRANSFORMS_H
+
+
+namespace ar_transforms
+{
+  typedef double Transform_T[3][4];
+  //Read in transforms representing marker frame wrt center frame from file name name.
+  //file must have givenObjNum number of transforms 
+  Transform_T *read_Transforms (char *name, int givenObjNum); 
+}
+#endif
